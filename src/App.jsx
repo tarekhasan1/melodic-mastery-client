4911f2208d086595582a3e45af3220c81a256545
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-06-08 03:46:15 +0600

    react bootstrap and bootstrap installed and router setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1 className='text-center text-success'>Title</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './routes/routes';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>,
+)
