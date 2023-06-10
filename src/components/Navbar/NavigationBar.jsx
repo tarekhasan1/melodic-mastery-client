5037934f088ab26d4f3d3198cd040aacd1437715
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-06-11 03:41:04 +0600

    logout implented successfully and made navbar conditional based on user availablity

--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -1,74 +1,116 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
-import './NavigationBar.css'
+import "./NavigationBar.css";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProviders";
 
 const NavigationBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+  console.log("nav user: ", user);
 
-    return (
-        <div
-        style={{
-          backgroundImage: "url('https://www.incisiv.com/hubfs/Events/Rock%20and%20Roll%20Underground%202021/Hero_over_bg_v3.gif')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-        >
-        <Navbar
-          collapseOnSelect
-          expand="lg"
-          className="background-color nav-position"
-          variant="dark"
-        >
-          <Container>
-            <Navbar.Brand className="text-secondary fancy-font fs-3" href="/"> <img className="logo-img" src='logo' alt="" /> <span className="text-white fw-bold fancy-font">M</span>elodic <span className="text-white fw-bold fancy-font">M</span>astery</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mx-auto ms-4">
-                <NavLink className={({ isActive }) => (isActive ? "active-route me-4" : "me-4")}
-                  to="/"
-                >
-                  Home
-                </NavLink>
-                <NavLink className={({ isActive }) => (isActive ? "active-route me-4" : "me-4")}
-                  to="/classes"
-                >
-                  Classes
-                </NavLink>
-                <NavLink className={({ isActive }) => (isActive ? "active-route me-4" : "me-4")}
+  const handleLogout = () => {
+    logOut()
+      .then()
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url('https://www.incisiv.com/hubfs/Events/Rock%20and%20Roll%20Underground%202021/Hero_over_bg_v3.gif')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="background-color nav-position"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand className="text-secondary fancy-font fs-3" href="/">
+            {" "}
+            <img
+              className="logo-img"
+              src="https://static.wixstatic.com/media/53928e_fdded28a0bb24dc0b5c197efecd3deae~mv2.png/v1/fill/w_578,h_577,al_c/53928e_fdded28a0bb24dc0b5c197efecd3deae~mv2.png"
+              alt=""
+            />{" "}
+            <span className="text-white fw-bold fancy-font">M</span>elodic{" "}
+            <span className="text-white fw-bold fancy-font">M</span>astery
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-route me-4" : "me-4"
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-route me-4" : "me-4"
+                }
+                to="/classes"
+              >
+                Classes
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-route me-4" : "me-4"
+                }
+                to="/instructor"
+              >
+                Instructor
+              </NavLink>
+              {user && (
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "active-route me-4" : "me-4"
+                  }
                   to="/dashboard"
                 >
-                  dashboard
+                  Dashboard
                 </NavLink>
-                <NavLink className={({ isActive }) => (isActive ? "active-route me-4" : "me-4")}
-                  to="/instructor"
-                >
-                  Instructor
-                </NavLink>
-              </Nav>
-              <Nav>
+              )}
+            </Nav>
+            <Nav>
+              {user ? (
                 <div>
                   <div className="d-flex align-items-center">
-                  <img
-                    className="img-control me-2"
-                    src="s.jpg"
-                    alt=""
-                    title='{user.displayName}'
-                  />
-                  <Link className="d-flex align-items-center text-decoration-none">
-                    <Button variant="secondary">
-                      Log Out
-                    </Button>
-                  </Link>
+                    <img
+                      className="img-control me-2"
+                      src={user.photoURL}
+                      alt=""
+                      title={user.displayName}
+                    />
+                    <Link className="d-flex align-items-center text-decoration-none">
+                      <Button
+                        onClick={handleLogout}
+                        variant="outline-secondary"
+                      >
+                        Log Out
+                      </Button>
+                    </Link>
                   </div>
                 </div>
+              ) : (
                 <Link to="/login">
-                  <Button variant="secondary">Log In</Button>
+                  <Button variant="outline-secondary">Log In</Button>
                 </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-    );
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default NavigationBar;
