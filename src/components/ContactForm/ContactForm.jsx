3e0696c71dcee5743ac55cf5431c23f9e66f9387
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-06-16 13:45:48 +0600

    animation used on contactus form at homepage

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import './ContactForm.css'
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "./ContactForm.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
+
+  useEffect(() => {
+    AOS.init(); // Initialize AOS
+  }, []);
 
   const handleChange = (e) => {
     setFormData({
@@ -22,9 +28,9 @@ const ContactForm = () => {
     console.log(formData);
     // Reset the form
     setFormData({
-      name: '',
-      email: '',
-      message: '',
+      name: "",
+      email: "",
+      message: "",
     });
   };
 
@@ -32,18 +38,27 @@ const ContactForm = () => {
     <div
       className="contact-form-container"
       style={{
-        backgroundImage: "url('https://www.seekpng.com/png/full/821-8215450_music-note-background-musical-note.png')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundImage:
+          "url('https://www.seekpng.com/png/full/821-8215450_music-note-background-musical-note.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <Container>
-      <h1 className='text-center mt-5 text-white'>Contact Us</h1>
-      <p className='text-center text-white mb-5'>If you have any questions or inquiry about us, you can simply email us to about anything related to our classes, instructors etc. Feel free to mail us!</p>
-        <Row className='col-container text-white'>
+        <h1 className="text-center mt-5 text-white">Contact Us</h1>
+        <p className="text-center text-white mb-5">
+          If you have any questions or inquiry about us, you can simply email us
+          to about anything related to our classes, instructors etc. Feel free
+          to mail us!
+        </p>
+        <Row className="col-container text-white">
           <Col md={6}>
-            <div className="form-container mt-5">
-              <h2 className='text-center'>Send A Message!</h2>
+            <div
+              className="form-container mt-5"
+              data-aos="flip-right"
+              data-aos-duration="2000"
+            >
+              <h2 className="text-center">Send A Message!</h2>
               <Form onSubmit={handleSubmit}>
                 <Form.Group>
                   <Form.Label>Name:</Form.Label>
@@ -75,13 +90,18 @@ const ContactForm = () => {
                     required
                   />
                 </Form.Group>
-                <Button className='btn btn-warning mt-3' type="submit">Send</Button>
+                <Button className="btn btn-warning mt-3" type="submit">
+                  Send
+                </Button>
               </Form>
             </div>
           </Col>
           <Col md={6}>
             <div className="picture-container">
-              <img src="https://cdni.iconscout.com/illustration/premium/thumb/contact-us-5795988-4849052.png?f=webp" alt="Contact" />
+              <img
+                src="https://cdni.iconscout.com/illustration/premium/thumb/contact-us-5795988-4849052.png?f=webp"
+                alt="Contact"
+              />
             </div>
           </Col>
         </Row>
